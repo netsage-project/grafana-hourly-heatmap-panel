4d650c4b8a1ce2b95f08df641a3ab3a0649a3de9
Mahesh Khanal <42851980+MaheshKhanal@users.noreply.github.com>
2020-08-28 00:25:00 -1000

    set default color to orange
    Added default color to orange.

--- src/module.ts
+++ src/module.ts
@@ -64,7 +64,7 @@ export const plugin = new PanelPlugin<HeatmapOptions, HeatmapFieldConfig>(Heatma
         .addColorPicker({
           path:'nullValueColor', 
           name:'Null Value Color', 
-          defaultValue:'red', 
+          defaultValue:'orange', 
         })
         .addSelect({
           path: 'colorSpace',
